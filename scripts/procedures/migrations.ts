5a49a9bc9a7f73c2ecde2b29479ba882842a4ef9
benthecarman <benthecarman@live.com>
2023-12-04 16:29:20 -0600

    Update mutiny to v0.4.39-1

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.4.39");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.4.39-1");
