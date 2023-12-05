9d515e0b6c6116cd65c0a9ffb1f210ebc7963350
Mariusz Kogen <k0gen@pm.me>
2023-12-05 15:52:05 +0100

    entrypoint refactor * version bump * clean manifest

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,38 +3,49 @@
 LN_PROXY_PORT=3001
 RUST_LOG=debug
 
-  POSTGRES_DATADIR="/var/lib/postgresql/15"
-  POSTGRES_CONFIG="/etc/postgresql/15"
+POSTGRES_DATADIR="/var/lib/postgresql/15"
+POSTGRES_CONFIG="/etc/postgresql/15"
+CLUSTER_NAME="main"
 
-  #Start and Configure PostgreSQL
-  echo 'Starting PostgreSQL database server for the first time...'
-  mkdir -p $POSTGRES_DATADIR $POSTGRES_CONFIG
-  mv /var/lib/main $POSTGRES_DATADIR
-  chown -R postgres:postgres $POSTGRES_DATADIR
-  chown -R postgres:postgres $POSTGRES_CONFIG
-  chmod -R 700 $POSTGRES_DATADIR
-  chmod -R 700 $POSTGRES_CONFIG
-  su - postgres -c "pg_createcluster 15 lib"
-  su - postgres -c "pg_ctlcluster 15 lib start"
+# Start and Configure PostgreSQL
+echo 'Starting PostgreSQL database server...'
 
-  POSTGRES_USER=postgres
-  POSTGRES_PASSWORD=docker
-  POSTGRES_DB=vss
+# Move main directory if it doesn't exist in the target
+if [ ! -d "$POSTGRES_DATADIR/$CLUSTER_NAME" ]; then
+  echo 'Moving PostgreSQL data directory...'
+  mkdir -p "$POSTGRES_DATADIR" "$POSTGRES_CONFIG"
+  mv /var/lib/main "$POSTGRES_DATADIR"
+else
+  echo "Target directory '$POSTGRES_DATADIR/$CLUSTER_NAME' already exists. Skipping move operation."
+fi
 
-  # Start db server
-  service postgresql start
-  echo 'Creating user...'
-  su - postgres -c "createuser $POSTGRES_USER"
-  echo 'Creating db...'
-  su - postgres -c "createdb $POSTGRES_DB"
-  echo 'Setting password...'
-  su - postgres -c 'psql -c "ALTER USER '$POSTGRES_USER' WITH ENCRYPTED PASSWORD '"'"$POSTGRES_PASSWORD"'"';"'
-  echo 'Granting db permissions...'
-  su - postgres -c 'psql -c "grant all privileges on database '$POSTGRES_DB' to '$POSTGRES_USER';"'
-  echo 'Creating .pgpass file...'
-  echo "localhost:5432:'$POSTGRES_USER':'$POSTGRES_PASSWORD'" > $POSTGRES_DATADIR/../.pgpass
-  chmod -R 0600 $POSTGRES_DATADIR/../.pgpass
-  chown postgres:postgres $POSTGRES_DATADIR/../.pgpass
+# Set ownership and permissions
+chown -R postgres:postgres "$POSTGRES_DATADIR" "$POSTGRES_CONFIG"
+chmod -R 700 "$POSTGRES_DATADIR" "$POSTGRES_CONFIG"
+
+# Start PostgreSQL cluster
+su - postgres -c "pg_ctlcluster 15 $CLUSTER_NAME start"
+
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=docker
+POSTGRES_DB=vss
+
+# Create user if not exists
+su - postgres -c "createuser $POSTGRES_USER" 2>/dev/null || true
+
+# Create database if not exists
+su - postgres -c "createdb --encoding=UTF8 $POSTGRES_DB" 2>/dev/null || true
+
+echo 'Setting password...'
+su - postgres -c 'psql -c "ALTER USER '$POSTGRES_USER' WITH ENCRYPTED PASSWORD '"'"$POSTGRES_PASSWORD"'"';"'
+
+echo 'Granting db permissions...'
+su - postgres -c 'psql -c "grant all privileges on database '$POSTGRES_DB' to '$POSTGRES_USER';"'
+
+echo 'Creating .pgpass file...'
+echo "localhost:5432:$POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD" >"$POSTGRES_DATADIR/../.pgpass"
+chmod 0600 "$POSTGRES_DATADIR/../.pgpass"
+chown postgres:postgres "$POSTGRES_DATADIR/../.pgpass"
 
 /app/vss-rs &
 /app/ln-websocket-proxy &
