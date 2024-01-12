7a8ffd57e779c60cd4ac9992e63533e666384f2f
benthecarman <benthecarman@live.com>
2024-01-12 14:39:35 +0000

    0.5.3

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.5.1");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.5.3");
