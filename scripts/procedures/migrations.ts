5fc454a59da18aa67216f641655e61be6627afe9
benthecarman <benthecarman@live.com>
2024-02-15 22:17:43 +0000

    Update to 0.5.9

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.5.7");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.5.9");
