7edbe6318a29767a1be1e6d76678e53d59d28e8b
benthecarman <benthecarman@live.com>
2024-04-29 12:53:47 -0500

    Bump to v0.6.8

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.6.5");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "0.6.8");
